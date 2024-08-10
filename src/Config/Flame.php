faeff97d7dd7ab78ced7ff3fa12f6861eeaf03c7
ysugimoto <sugimoto@wnotes.net>
2024-08-11 02:37:06 +0900

    Install CI/CD

--- src/Config/Flame.php
+++ src/Config/Flame.php
@@ -17,7 +17,7 @@ class Flame extends BaseConfig
 {
     /**
      * Configuration for the public directory path.
-     * Typically the CodeIgniter's public path is defined as PUBLICPATH consant
+     * Typically the CodeIgniter's public path is /public
      * but you can change arbitrary path - for example, you don't want to place .flame file in public directory.
      * But then you need to change the manifest generation configuration on flame-vite-plugin configuration.
      *
@@ -28,7 +28,7 @@ class Flame extends BaseConfig
 
     /**
      * Configuration for the flame manifest file that created by flame-vite-plugin.
-     * Typically manifest file will be generated into PUBLICPATH/.flame
+     * Typically manifest file will be generated into /public/.flame
      *
      * @access public
      * @property string $manifestFile
